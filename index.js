6695c21768c192cc5437bd82d0d4f4df84fd2299
abhisinojia <63142248+abhisinojia@users.noreply.github.com>
2021-03-24 18:07:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var buttonNo = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < buttonNo; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+     var button = this.innerHTML;
+     play(button);
+     animation(button);
+
+  });
+}
+
+document.addEventListener("keypress" , function(){
+  play(event.key);
+  animation(event.key);
+});
+
+function play(key){
+
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+     case "a":
+       var audio = new Audio("sounds/kick-bass.mp3");
+       audio.play();
+       break;
+     case "s":
+       var audio = new Audio("sounds/snare.mp3");
+       audio.play();
+       break;
+     case "d":
+       var audio = new Audio("sounds/tom-1.mp3");
+       audio.play();
+       break;
+     case "j":
+       var audio = new Audio("sounds/tom-2.mp3");
+       audio.play();
+       break;
+     case "k":
+       var audio = new Audio("sounds/tom-3.mp3");
+       audio.play();
+       break;
+     case "l":
+       var audio = new Audio("sounds/tom-4.mp3");
+       audio.play();
+       break;
+    default:
+  }
+};
+
+function animation(currentkey){
+
+  var activeButton = document.querySelector("."+currentkey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){activeButton.classList.remove("pressed")}, 100);
+
+};
